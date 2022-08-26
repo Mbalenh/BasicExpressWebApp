336f0a3b2b8f1842dcba6f5fd154cbb21ee4fbbd
Mbalenh <3408534@myuwc.ac.za>
2022-08-26 13:16:15 +0200

    added modifield files

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const ProductService = require('./services/product-service');
 const UserService = require('./services/user-service');
 const pgp = require('pg-promise')();
 
-const DATABASE_URL= process.env.DATABASE_URL || "postgresql://localhost:5432/my_products_list";
+const DATABASE_URL= process.env.DATABASE_URL ||"postgresql://coder:coder123@localhost:5432/my_products"
 
 const config = { 
 	connectionString : DATABASE_URL
